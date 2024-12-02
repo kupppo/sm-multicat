37d1129c9a46a57878a530dedd58150b19ea781d
kupppo <kupppo@protonmail.com>
2024-12-02 11:46:44 +0000

    add in suspense for client side hook

--- app/login/user-select.tsx
+++ app/login/user-select.tsx
@@ -15,6 +15,7 @@ import { AnimatedNamePill } from '../setup/username'
 import { cn } from '@/lib/utils'
 import { sendLoginLink } from '../actions/setup'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 export default function UserSelect({ users = [] }: { users: any[] }) {
   const searchParams = useSearchParams()
@@ -41,61 +42,63 @@ export default function UserSelect({ users = [] }: { users: any[] }) {
 
     return (
       <div className="min-h-[350px]">
-        <h1 className="text-2xl mb-8">Send Login Link</h1>
-        <div className="flex flex-col gap-y-8">
-          <div className="flex flex-col">
-            <div className="w-auto mx-auto">
-              <AnimatedNamePill name={userData.name} />
+        <Suspense fallback={null}>
+          <h1 className="text-2xl mb-8">Send Login Link</h1>
+          <div className="flex flex-col gap-y-8">
+            <div className="flex flex-col">
+              <div className="w-auto mx-auto">
+                <AnimatedNamePill name={userData.name} />
+              </div>
+              <div className={cn('w-auto mx-auto', isDisabled && 'invisible')}>
+                <Button
+                  variant="ghost"
+                  className="leading-none opacity-40"
+                  size="sm"
+                  onClick={() => setSelectedUser(null)}
+                >
+                  Cancel
+                </Button>
+              </div>
             </div>
-            <div className={cn('w-auto mx-auto', isDisabled && 'invisible')}>
+            <div>
               <Button
-                variant="ghost"
-                className="leading-none opacity-40"
-                size="sm"
-                onClick={() => setSelectedUser(null)}
+                disabled={isDisabled}
+                onClick={() => handleSubmit(selectedUser)}
               >
-                Cancel
+                <span>
+                  {(state === 'idle' || state === 'error') && 'Send login link'}
+                  {state === 'submitting' && (
+                    <div className="relative">
+                      <span className="invisible">Send login link</span>
+                      <div className="w-full h-full flex absolute top-0 left-0 justify-center">
+                        <LoaderCircle className="animate-spin w-2 h-2" />
+                      </div>
+                    </div>
+                  )}
+                  {state === 'success' && 'Login link sent'}
+                </span>
               </Button>
+              <p className="mt-8 text-sm text-center max-w-[300px] mx-auto">
+                {state === 'idle' && (
+                  <>
+                    This will send a DM to you from{' '}
+                    <span className="font-semibold">Inertia</span> on Discord with
+                    a link to login.
+                  </>
+                )}
+                {state === 'submitting' && <>Sending login link...</>}
+                {state === 'success' && (
+                  <>Please check your DMs for the login link sent.</>
+                )}
+              </p>
+              {state === 'error' && (
+                <p className="mt-8 text-sm text-center max-w-[300px] mx-auto text-red-700">
+                  An error occured. Please try again.
+                </p>
+              )}
             </div>
           </div>
-          <div>
-            <Button
-              disabled={isDisabled}
-              onClick={() => handleSubmit(selectedUser)}
-            >
-              <span>
-                {(state === 'idle' || state === 'error') && 'Send login link'}
-                {state === 'submitting' && (
-                  <div className="relative">
-                    <span className="invisible">Send login link</span>
-                    <div className="w-full h-full flex absolute top-0 left-0 justify-center">
-                      <LoaderCircle className="animate-spin w-2 h-2" />
-                    </div>
-                  </div>
-                )}
-                {state === 'success' && 'Login link sent'}
-              </span>
-            </Button>
-            <p className="mt-8 text-sm text-center max-w-[300px] mx-auto">
-              {state === 'idle' && (
-                <>
-                  This will send a DM to you from{' '}
-                  <span className="font-semibold">Inertia</span> on Discord with
-                  a link to login.
-                </>
-              )}
-              {state === 'submitting' && <>Sending login link...</>}
-              {state === 'success' && (
-                <>Please check your DMs for the login link sent.</>
-              )}
-            </p>
-            {state === 'error' && (
-              <p className="mt-8 text-sm text-center max-w-[300px] mx-auto text-red-700">
-                An error occured. Please try again.
-              </p>
-            )}
-          </div>
-        </div>
+        </Suspense>
       </div>
     )
   }
