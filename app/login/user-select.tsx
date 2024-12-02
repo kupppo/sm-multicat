bfe65f7b19ba431d1d41d2ab6c7e79501a721ec0
kupppo <kupppo@protonmail.com>
2024-12-02 12:07:16 +0000

    fix search for login

--- app/login/user-select.tsx
+++ app/login/user-select.tsx
@@ -103,7 +103,14 @@ export default function UserSelect({ users = [] }: { users: any[] }) {
   return (
     <div className="min-h-[350px] flex flex-col">
       <h1 className="text-2xl mb-4">Send Login Link</h1>
-      <Command className="rounded-lg border shadow-md md:min-w-[450px] outline-none">
+      <Command
+        className="rounded-lg border shadow-md md:min-w-[450px] outline-none"
+        filter={(_value: string, search: string, keywords: string[]) => {
+          const extendValue = keywords.join(' ')
+          if (extendValue.includes(search)) return 1
+          return 0
+        }}
+      >
         <CommandInput placeholder="Enter your Discord username..." autoFocus />
         <CommandList>
           <CommandEmpty>No user found.</CommandEmpty>
@@ -112,6 +119,7 @@ export default function UserSelect({ users = [] }: { users: any[] }) {
               <CommandItem
                 key={user.id}
                 value={user.id}
+                keywords={[user.name]}
                 onSelect={() => {
                   setSelectedUser(user.id)
                 }}
